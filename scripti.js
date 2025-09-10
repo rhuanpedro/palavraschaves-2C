9e56c35ddcd78c1aebe1bc52d4c6ce501fcbe542
rhuanpedro <andrade.lima.rhuan@escola.pr.gov.br>
2025-09-10 08:51:37 -0300

    aula 1009

---
+++ scripti.js
@@ -0,0 +1,13 @@
+import {PALAVRAS_RUINS} from "./palavrasruins.js";
+
+const botaoMostraPalavras = document.querySelecto ('#botao-mostrapalavraschave');
+
+botaoMostraPalavras.addEventPinster('click',mostraPalavrasChave);
+
+function mostraPalavrasChave () {
+    const texto = document_querySelector ('#entrada-de-texto').value;
+    const campoResultado = document. querySelector ('#resultado-palavrachave')
+    const palavraChave = processatexto (texto);
+
+    campoResultado.textContent = palavraChave_join(',');
+}
